968cf1fb11ac7afc2a2ac88f98f8aee3f1fef171
hernawana60 <hernawana60@gmail.com>
2021-11-01 00:54:16 +0700

    love merah

--- src/pages/login.js
+++ src/pages/login.js
@@ -36,19 +36,19 @@ export default function Login() {
         <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
       </div>
       <div className="flex flex-col w-2/5">
-        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-300 mb-4 rounded">
           <h1 className="flex justify-center w-full">
             <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
           </h1>
 
-          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+          {error && <p className="mb-4 text-xs text-red-400">{error}</p>}
 
           <form onSubmit={handleLogin} method="POST">
             <input
               aria-label="Enter your email address"
               type="text"
               placeholder="Email address"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-300 rounded mb-2"
               onChange={({ target }) => setEmailAddress(target.value)}
               value={emailAddress}
             />
@@ -56,14 +56,14 @@ export default function Login() {
               aria-label="Enter your password"
               type="password"
               placeholder="Password"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-300 rounded mb-2"
               onChange={({ target }) => setPassword(target.value)}
               value={password}
             />
             <button
               disabled={isInvalid}
               type="submit"
-              className={`bg-blue-medium text-white w-full rounded h-8 font-bold
+              className={`bg-blue-600 text-white w-full rounded h-8 font-bold
             ${isInvalid && 'opacity-50'}`}
             >
               Login
@@ -73,7 +73,7 @@ export default function Login() {
         <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
           <p className="text-sm">
             Don't have an account?{` `}
-            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
+            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-500">
               Sign up
             </Link>
           </p>
